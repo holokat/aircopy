31d1eb9933e67ce4d7ddb7225ce08ff6fd2ba9a1
karnagebitcoin <131526701+karnagebitcoin@users.noreply.github.com>
2026-03-10 15:12:19 +0900

    Point site to GitHub repo

--- website/app.js
+++
@@ -1,5 +0,0 @@
-const githubLink = document.querySelector("[data-github-link]");
-
-if (githubLink && window.location.hostname.endsWith("netlify.app")) {
-  githubLink.setAttribute("href", "https://github.com/");
-}
